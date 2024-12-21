d7b34f66a0233fd334a1fe4afa1e53b4fdde1e1c
Sanduntha <sanduntharaka9651@gmail.com>
2024-12-21 16:11:02 +0530

    Register page done

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,3 +1,4 @@
+import Register from '../pages/RegisterPage/Register'
 import './App.css'
 
 function App() {
@@ -5,7 +6,7 @@ function App() {
 
   return (
     <>
-      
+      <Register/>
     </>
   )
 }
